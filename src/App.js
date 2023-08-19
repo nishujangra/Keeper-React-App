0212600927209291b37cf66b39a9dab73f8eadcd
nishujangra <ndjangra1027@gmail.com>
2023-08-19 15:51:56 +0530

    level 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <div className="note">
+        <h1>This is the note title</h1>
+        <p>This is the note content</p>
+      </div>
+      
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="text-muted">Copyright ⓒ  {new Date().getFullYear()}</p>
+        </footer>
+    );
+}
+
+export default Footer;
